e4370a0a0b3aa3ebf3d9c57d6d07caffb798ff97
Matty <wu.hang.gcu@qq.com>
2021-09-23 23:07:49 +0800

    clean directorys of projects that was deleted in Eclipse

--- 20-21SummerHoliday/chapter10/part15_2/Example10_21.java
+++
@@ -1,9 +0,0 @@
-package part15_2;
-
-public class Example10_21 {
-
-	public static void main(String[] args) {
-		new CommFrame();
-	}
-
-}
--- HuaRongRoad/src/chapter9/part11/Example9_25.java
+++
@@ -1,9 +0,0 @@
-package chapter9.part11;
-
-public class Example9_25 {
-
-	public static void main(String[] args) {
-		new HuaRongRoad().setTitle("»ªÈÝµÀ");
-	}
-
-}
