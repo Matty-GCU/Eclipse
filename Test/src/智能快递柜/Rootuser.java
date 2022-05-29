c272c0ff79b47e9cd034037007fce80bda2ac009
Matty <wu.hang.gcu@qq.com>
2022-05-29 09:56:50 +0800

    2022/05/29 周日 common backup

---
+++ Test/src/智能快递柜/Rootuser.java
@@ -0,0 +1,12 @@
+package ÖÇÄÜ¿ìµÝ¹ñ;
+
+
+public class Rootuser {
+     String phoneNumber;
+     int password;
+    public  Rootuser(String phoneNumber,int password) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+}
